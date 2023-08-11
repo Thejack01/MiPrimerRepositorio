ed8b9e5313b9ee654c3e4406099b9f75619cb97a
daniel <130762163+Thejack01@users.noreply.github.com>
2023-08-11 10:14:51 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+
+
+/*
+ * @author josedaniel carvajal
+ */
+
+//clase principal de java 
+public class HolaMundo {
+   public static void main (String[] args) {
+       System.out.println("hola jose");
+   }
+   
+}
